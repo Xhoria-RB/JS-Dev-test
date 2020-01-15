452ba7eee6be92b594b10e333629b2341a36b11c
Ricardo <shirotokuro3715@gmail.com>
2020-01-15 17:13:36 -0400

    Add update and delete

--- src/Components/Contacts.js
+++ src/Components/Contacts.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -7,7 +7,10 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Button from '@material-ui/core/Button';
 import DeleteIcon from '@material-ui/icons/Delete';
-import EditIcon from '@material-ui/icons/Edit';
+import UserContext from './UserContext';
+import UpdateContacts from './UpdateContact';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+
 
 const useStyles = makeStyles(theme => ({
   button: {
@@ -17,6 +20,13 @@ const useStyles = makeStyles(theme => ({
 
 export default function Contacts({ data, classes }) {
   const btnClass = useStyles();
+  const { deleteContact } = useContext(UserContext);
+
+  const handleDelete = (id) => {
+    if (window.confirm('Do you really want to DELETE this contact?')) {
+      deleteContact(id);
+    }
+  };
   return (
     <Table className={classes.table} aria-label="simple table">
       <TableHead>
@@ -35,12 +45,18 @@ export default function Contacts({ data, classes }) {
             <TableCell align="left">{row.name}</TableCell>
             <TableCell align="left">{row.email}</TableCell>
             <TableCell align="left">{row.phone}</TableCell>
-            <Button variant="contained" color="primary" className={btnClass.button} aria-label="edit" startIcon={<EditIcon />}>
-              Edit
+            <ButtonGroup size="small" aria-label="small outlined button group">
+              <UpdateContacts id={row.id} btnClass={btnClass} />
+              <Button
+                variant="contained"
+                color="default"
+                className={btnClass.button}
+                onClick={() => handleDelete(row.id)}
+                aria-label="delete"
+                startIcon={<DeleteIcon />}>
+                Delete
             </Button>
-            <Button variant="contained" color="primary" className={btnClass.button} aria-label="delete" startIcon={<DeleteIcon />}>
-              Delete
-            </Button>
+            </ ButtonGroup>
           </TableRow>
         ))}
       </TableBody>
@@ -48,3 +64,67 @@ export default function Contacts({ data, classes }) {
 
   );
 }
+
+
+// import React, { useState } from 'react';
+// import remove from 'lodash/remove';
+// import { makeStyles } from '@material-ui/core/styles';
+// import Table from '@material-ui/core/Table';
+// import TableBody from '@material-ui/core/TableBody';
+// import TableCell from '@material-ui/core/TableCell';
+// import TableHead from '@material-ui/core/TableHead';
+// import TableRow from '@material-ui/core/TableRow';
+// import Button from '@material-ui/core/Button';
+// import DeleteIcon from '@material-ui/icons/Delete';
+// import EditIcon from '@material-ui/icons/Edit';
+
+// const useStyles = makeStyles(theme => ({
+//   button: {
+//     margin: theme.spacing(1),
+//   },
+// }));
+
+// export default function Contacts({ data, classes }) {
+
+//   const [contacts, setContacts] = useState(data)
+
+//   const handleDelete = (id) => {
+//     const newContacts = remove(contacts, (contact) => {
+//       return contact.id !== id
+//     });
+
+//     setContacts(newContacts)
+//   };
+
+//   const btnClass = useStyles();
+//   return (
+//     <Table className={classes.table} aria-label="simple table">
+//       <TableHead>
+//         <TableRow>
+//           <TableCell align="left">ID</TableCell>
+//           <TableCell align="left">Name</TableCell>
+//           <TableCell align="left">Email</TableCell>
+//           <TableCell align="left">Phone number</TableCell>
+//           <TableCell align="center">Actions</TableCell>
+//         </TableRow>
+//       </TableHead>
+//       <TableBody>
+//         {contacts.map(row => (
+//           <TableRow key={row.name}>
+//             <TableCell align="left">{row.id}</TableCell>
+//             <TableCell align="left">{row.name}</TableCell>
+//             <TableCell align="left">{row.email}</TableCell>
+//             <TableCell align="left">{row.phone}</TableCell>
+//             <Button variant="contained" color="primary" className={btnClass.button} aria-label="edit" startIcon={<EditIcon />}>
+//               Edit
+//             </Button>
+//             <Button onClick={() => handleDelete(row.id)} variant="contained" color="primary" className={btnClass.button} aria-label="delete" startIcon={<DeleteIcon />}>
+//               Delete
+//             </Button>
+//           </TableRow>
+//         ))}
+//       </TableBody>
+//     </Table>
+
+//   );
+// }
--- src/Components/UserContext.js
+++ src/Components/UserContext.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import remove from 'lodash/remove';
 
 const UserContext = React.createContext({});
 
@@ -16,13 +17,30 @@ class UserProvider extends Component {
       const holder = this.state.users.concat(contact)
       this.setState({ users: holder })
     }
+    this.updateContact = (id, data) => {
+      const arrUsers = this.state.users
+      const index = arrUsers.findIndex(el => el.id === id);
+      arrUsers[index] = { ...arrUsers[index], ...data };
+      this.setState({ users: arrUsers })
+
+    }
+
+    this.deleteContact = (id) => {
+      const arrUsers = this.state.users;
+      remove(arrUsers, (el) => el.id === id);
+      this.setState({ users: arrUsers });
+    }
 
   }
 
   render() {
     return (
       <UserContext.Provider value={{
-        state: this.state.users, fillUsers: this.fillUsers, addContact: this.addContact
+        state: this.state.users,
+        fillUsers: this.fillUsers,
+        addContact: this.addContact,
+        updateContact: this.updateContact,
+        deleteContact: this.deleteContact
       }}
       >
         {this.props.children}
